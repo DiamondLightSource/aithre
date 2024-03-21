d50698d332587c2a9da16cfdf33a03008f6ffd1c
co2e14 <christian.orr@diamond.ac.uk>
2024-03-21 16:12:01 +0000

    format in black

--- bin/guiv4_prod.py
+++ bin/guiv4_prod.py
@@ -19,18 +19,23 @@ import pickle
 version = "4.2.5"
 # Set grid/beam position/scale.
 line_width = 2
-line_spacing = 115 # depends on pixel size, 60 for MANTA507B
-line_color = (140, 140, 140) #greyness
+line_spacing = 115  # depends on pixel size, 60 for MANTA507B
+line_color = (140, 140, 140)  # greyness
 beamX = 2276
 beamY = 1320
 feed_width = int(ca.caget(pv.oav_max_x))
 display_width = 2012
 display_height = 1518
-camera_pixel_size = 1.85 # Alvium1240M
+camera_pixel_size = 1.85  # Alvium1240M
 feed_display_ratio = feed_width / display_width
-calibrate = (camera_pixel_size / feed_display_ratio) / 1000 # play around with the end number to find correct
+calibrate = (
+    camera_pixel_size / feed_display_ratio
+) / 1000  # play around with the end number to find correct
 
-print(f"Feed to display ratio is {str(feed_display_ratio)} so calibrate value for pixel size of {str(camera_pixel_size)}um should be {str(calibrate)}")
+print(
+    f"Feed to display ratio is {str(feed_display_ratio)} so calibrate value for pixel size of {str(camera_pixel_size)}um should be {str(calibrate)}"
+)
+
 
 # separate thread for OAV
 class OAVThread(QtCore.QThread):
@@ -48,7 +53,9 @@ class OAVThread(QtCore.QThread):
 
     def run(self):
         self.ThreadActive = True
-        self.cap = cv.VideoCapture("http://bl23i-ea-serv-01.diamond.ac.uk:8080/OAV.mjpg.mjpg")
+        self.cap = cv.VideoCapture(
+            "http://bl23i-ea-serv-01.diamond.ac.uk:8080/OAV.mjpg.mjpg"
+        )
         while self.ThreadActive:
             ret, frame = self.cap.read()
             if self.ThreadActive and ret:
@@ -59,10 +66,10 @@ class OAVThread(QtCore.QThread):
 
                 cv.line(
                     frame,
-                    (beamX - 20, beamY), #bigness
+                    (beamX - 20, beamY),  # bigness
                     (beamX + 20, beamY),
-                    (0, 255, 0), #color
-                    3, # thickness
+                    (0, 255, 0),  # color
+                    3,  # thickness
                 )
                 cv.line(
                     frame,
@@ -81,18 +88,22 @@ class OAVThread(QtCore.QThread):
                     x2 = min(self.beamX + new_width // 2, frame.shape[1])
                     y2 = min(self.beamY + new_height // 2, frame.shape[0])
 
-                    x1, x2 = self.adjust_roi_boundaries(x1, x2, frame.shape[1], new_width)
-                    y1, y2 = self.adjust_roi_boundaries(y1, y2, frame.shape[0], new_height)
+                    x1, x2 = self.adjust_roi_boundaries(
+                        x1, x2, frame.shape[1], new_width
+                    )
+                    y1, y2 = self.adjust_roi_boundaries(
+                        y1, y2, frame.shape[0], new_height
+                    )
 
                     cropped_frame = frame[y1:y2, x1:x2]
 
                     frame = cv.resize(cropped_frame, (frame.shape[1], frame.shape[0]))
 
-
-
                 # cv.ellipse(frame, (beamX, beamY), (12, 8), 0.0, 0.0, 360, (0,0,255), thickness=2) # could use to draw cut...
                 # cv.putText(frame,'text',bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
-                rgbImage = cv.cvtColor(frame, cv.COLOR_BGR2RGB) # color because of crosshair, drawings etc
+                rgbImage = cv.cvtColor(
+                    frame, cv.COLOR_BGR2RGB
+                )  # color because of crosshair, drawings etc
                 convertToQtFormat = QtGui.QImage(
                     rgbImage.data,
                     rgbImage.shape[1],
@@ -100,7 +111,9 @@ class OAVThread(QtCore.QThread):
                     QtGui.QImage.Format_RGB888,
                 )
                 p = convertToQtFormat
-                p = convertToQtFormat.scaled(display_width, display_height, QtCore.Qt.KeepAspectRatio) # numbers are a fraction of full res as full res is too big to fit on screen
+                p = convertToQtFormat.scaled(
+                    display_width, display_height, QtCore.Qt.KeepAspectRatio
+                )  # numbers are a fraction of full res as full res is too big to fit on screen
                 self.ImageUpdate.emit(p)
 
     def adjust_roi_boundaries(self, start, end, max_value, window_size):
@@ -108,7 +121,7 @@ class OAVThread(QtCore.QThread):
             end -= start
             start = 0
         if end > max_value:
-            start -= (end - max_value)
+            start -= end - max_value
             end = max_value
         if (end - start) < window_size and (start + window_size) <= max_value:
             end = start + window_size
@@ -120,6 +133,7 @@ class OAVThread(QtCore.QThread):
     def stop(self):
         self.ThreadActive = False
         self.cap.release()
+
 
 # separate thread to run caget for RBVs
 class RBVThread(QtCore.QThread):
@@ -149,6 +163,7 @@ class RBVThread(QtCore.QThread):
             self.rbvUpdate.emit(allRBVsList)
             print(f"lRBVThreadComplete {str(datetime.now())}")
 
+
 class robotCheckThread(QtCore.QThread):
     robotUpdate = QtCore.pyqtSignal(list)
 
@@ -158,6 +173,7 @@ class robotCheckThread(QtCore.QThread):
             robotUpdateList = []
             robotUpdateList += [str(ca.caget(pv.robot_prog_running))]
             self.robotUpdate.emit(robotUpdateList)
+
 
 # class beamlineSafeThread(QtCore.QThread):
 #     beamlineSafe = QtCore.pyqtSignal(list)
@@ -196,7 +212,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.sliderGain.setProperty(
             "value", str(round(float(ca.caget(pv.oav_cam_gain_rbv))))
         )
-        #OAV zoom setup
+        # OAV zoom setup
         self.ui.sliderZoom.valueChanged.connect(self.handleZoom)
         # OAV connections thread
         self.zoomLevel = 1
@@ -223,7 +239,9 @@ class MainWindow(QtWidgets.QMainWindow):
         # th4.start()
         # gonio rotation buttons
         self.ui.buttonSlowOmegaTurn.clicked.connect(lambda: ca.caput(pv.omega_velo, 15))
-        self.ui.buttonFastOmegaTurn.clicked.connect(lambda: ca.caput(pv.omega_velo, 150))
+        self.ui.buttonFastOmegaTurn.clicked.connect(
+            lambda: ca.caput(pv.omega_velo, 150)
+        )
         self.ui.plusMinus3600.clicked.connect(self.goTopm3600)
         self.ui.minus180.clicked.connect(lambda: self.gonioRotate(-180))
         self.ui.plus180.clicked.connect(lambda: self.gonioRotate(180))
@@ -405,7 +423,13 @@ class MainWindow(QtWidgets.QMainWindow):
         arr = np.frombuffer(data, dtype=np.uint8).reshape((height, width, 3))
         options = QtWidgets.QFileDialog.Options()
         options |= QtWidgets.QFileDialog.DontUseNativeDialog
-        file_name, _ = QtWidgets.QFileDialog.getSaveFileName(self.ui.centralwidget,"QFileDialog.getSaveFileName()","","JPEG Files (*.jpg);;All Files (*)", options=options)
+        file_name, _ = QtWidgets.QFileDialog.getSaveFileName(
+            self.ui.centralwidget,
+            "QFileDialog.getSaveFileName()",
+            "",
+            "JPEG Files (*.jpg);;All Files (*)",
+            options=options,
+        )
 
         if file_name:
             _, file_extension = os.path.splitext(file_name)
@@ -431,7 +455,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def updateRBVs(self, rbvs):
         # stagez, gony, gonz, omega, oavexp, oavgain, currentsamp, goniosens, stagex, stagey
-        self.ui.stagez_rbv.setText(str(round(float(rbvs[0]), 3))) # used to be x now is z
+        self.ui.stagez_rbv.setText(
+            str(round(float(rbvs[0]), 3))
+        )  # used to be x now is z
         self.ui.gony_rbv.setText(str(round(float(rbvs[1]), 3)))
         self.ui.gonz_rbv.setText(str(round(float(rbvs[2]), 3)))
         # stop -0.0 to 0.0 jitter on GUI
@@ -469,22 +495,36 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.indicatorBeamlineSafe.setStyleSheet("background-color: red")
         else:
             pass
-        
+
     def autoCenter(self):
-        cap = cv.VideoCapture("http://bl23i-ea-serv-01.diamond.ac.uk:8080/OAV.mjpg.mjpg")
+        cap = cv.VideoCapture(
+            "http://bl23i-ea-serv-01.diamond.ac.uk:8080/OAV.mjpg.mjpg"
+        )
         ret, frame = cap.read()
-        if ret:            
-            filename = os.path.join(os.path.dirname(os.getcwd()), "captures", "autoCenter", f"{datetime.now().strftime('%d%m%y_%H%M%S')}.jpg")
+        if ret:
+            filename = os.path.join(
+                os.path.dirname(os.getcwd()),
+                "captures",
+                "autoCenter",
+                f"{datetime.now().strftime('%d%m%y_%H%M%S')}.jpg",
+            )
             cv.imwrite(filename, frame)
         # check if murko running
         # send image to murko and get info
         try:
             request_arguments = {}
             request_arguments["to_predict"] = str(filename)
-            request_arguments['model_img_size'] = (display_height, display_width)
-            request_arguments['save'] = True
-            request_arguments['min_size'] = 64
-            request_arguments['description'] = ['foreground', 'crystal', 'loop_inside', 'loop', ['crystal', 'loop'], ['crystal', 'loop', 'stem']]
+            request_arguments["model_img_size"] = (display_height, display_width)
+            request_arguments["save"] = True
+            request_arguments["min_size"] = 64
+            request_arguments["description"] = [
+                "foreground",
+                "crystal",
+                "loop_inside",
+                "loop",
+                ["crystal", "loop"],
+                ["crystal", "loop", "stem"],
+            ]
             context = zmq.context()
             socket = context.socket(zmq.REQ)
             socket.connect("tcp://bl23i-ea-serv-01.diamond.ac.uk:8901")
@@ -494,16 +534,14 @@ class MainWindow(QtWidgets.QMainWindow):
             print("Prediction successfull")
         except:
             print("Could not predict")
-        
+
         # move stage to center
         # rotate 90 and repeat
-        
-            
-            
-        
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     mainWin = MainWindow()
     mainWin.show()
